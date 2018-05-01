4ff447962c49162c7e48c958e2189f5d0647f689
Ben Brewer <benbrewerbowman@gmail.com>
2018-04-30 19:32:56 -0600

    Developer skill icons added

---
+++ src/_components/SkillChip/SkillChip.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const SkillChip = (props) => (
+    <div>
+        {/* logo */}
+        {/* logo name */}
+        {props.icon}
+
+        React
+    </div>
+);
+
+export { SkillChip };
