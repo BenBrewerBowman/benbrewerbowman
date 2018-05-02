b56a4f6f6838a581cd20228b49e91069ebba6ed0
Ben Brewer <benbrewerbowman@gmail.com>
2018-05-02 07:58:11 -0600

    Added visibility library, animation to contact form

--- src/_components/ContactPage/ContactPage.js
+++ src/_components/ContactPage/ContactPage.js
@@ -5,6 +5,7 @@ import Slide from 'material-ui/transitions/Slide';
 import Snackbar from 'material-ui/Snackbar';
 import IconButton from 'material-ui/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import OnVisible from 'react-on-visible';
 
 import { Button, Form, Message } from 'semantic-ui-react';
 
@@ -45,9 +46,18 @@ class ContactPage extends React.Component {
             subject: '', 
             messageBody: '',
             submitSuccess: false,
-            submitError: false
+            submitError: false,
+            visible: false
         }
     };
+
+    handleVisibility = () => {
+        if (!this.state.visible) {
+            this.setState({
+                visible: true
+            });
+        }
+    }
 
     handleChange = (e, { name, value }) => {
         this.setState({ [name]: value, submitError: false });
@@ -100,81 +110,84 @@ class ContactPage extends React.Component {
 
 
     render() {
-        const { name, senderEmail, subject, messageBody, submitSuccess, submitError } = this.state;
+        const { name, senderEmail, subject, messageBody, submitSuccess, submitError, visible } = this.state;
 
         return (
             <div style={{backgroundColor: 'whiteSmoke'}} id={this.props.id} > 
                 <div style={styles.container} >
                     <h1 style={styles.header}> Don't be shy! Drop me a line! </h1>
-                    <Form style={styles.form} onSubmit={this.handleSubmit} error={submitError}>
-                        <Form.Input 
-                            style={styles.field}
-                            required 
-                            label='Name' 
-                            placeholder='Ex. John Doe' 
-                            name='name' 
-                            value={name} 
-                            onChange={this.handleChange}
-                        />
-                        <Form.Input 
-                            style={styles.field} 
-                            label='Your Email'
-                            placeholder='Ex. email-address@some-site.com'
-                            name='senderEmail'
-                            value={senderEmail}
-                            onChange={this.handleChange}
-                        />
-                        <Form.Input 
-                            style={styles.field} 
-                            required
-                            label='Subject' 
-                            placeholder='Subject' 
-                            name='subject' 
-                            value={subject} 
-                            onChange={this.handleChange}
-                        />
-    
-                        <Form.TextArea 
-                            style={{...styles.field, minHeight: 150}} 
-                            autoHeight 
-                            required
-                            label='Message'
-                            placeholder='Your Message' 
-                            name='messageBody' value={messageBody} 
-                            onChange={this.handleChange}
-                        />
-                        <Button type='submit'>Submit</Button>
-                        <Slide in={submitError} direction="up" mountOnEnter unmountOnExit>
-                            <Message
-                                style={{textAlign: 'center'}}
-                                error
-                                header='Error sending message'
-                                content={'Sorry for the inconvenience! We have been notified and will take care of this problem promptly!'}
-                            />
+                    <OnVisible onChange={this.handleVisibility}>
+                        <Slide in={visible} direction="down" {...(visible ? { timeout: 750 } : {})} >
+                            <Form style={styles.form} onSubmit={this.handleSubmit} error={submitError}>
+                                <Form.Input 
+                                    style={styles.field}
+                                    required 
+                                    label='Name' 
+                                    placeholder='Ex. John Doe' 
+                                    name='name' 
+                                    value={name} 
+                                    onChange={this.handleChange}
+                                />
+                                <Form.Input 
+                                    style={styles.field} 
+                                    label='Your Email'
+                                    placeholder='Ex. email-address@some-site.com'
+                                    name='senderEmail'
+                                    value={senderEmail}
+                                    onChange={this.handleChange}
+                                />
+                                <Form.Input 
+                                    style={styles.field} 
+                                    required
+                                    label='Subject' 
+                                    placeholder='Subject' 
+                                    name='subject' 
+                                    value={subject} 
+                                    onChange={this.handleChange}
+                                />
+            
+                                <Form.TextArea 
+                                    style={{...styles.field, minHeight: 150}} 
+                                    autoHeight 
+                                    required
+                                    label='Message'
+                                    placeholder='Your Message' 
+                                    name='messageBody' value={messageBody} 
+                                    onChange={this.handleChange}
+                                />
+                                <Button type='submit'>Submit</Button>
+                                <Slide in={submitError} direction="up" mountOnEnter unmountOnExit>
+                                    <Message
+                                        style={{textAlign: 'center'}}
+                                        error
+                                        header='Error sending message'
+                                        content={'Sorry for the inconvenience! We have been notified and will take care of this problem promptly!'}
+                                    />
+                                </Slide>
+                                <Snackbar
+                                    style={{textAlign: 'center'}}
+                                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center'}}
+                                    open={submitSuccess}
+                                    autoHideDuration={6000}
+                                    onClose={this.handleSnackbarClose}
+                                    SnackbarContentProps={{
+                                        'aria-describedby': 'message-id',
+                                    }}
+                                    message={<span id="message-id">Thanks for reaching out!</span>}
+                                    action={[
+                                        <IconButton
+                                        key="close"
+                                        aria-label="Close"
+                                        color="inherit"
+                                        onClick={this.handleSnackbarClose}
+                                        >
+                                        <CloseIcon />
+                                        </IconButton>,
+                                    ]}
+                                />
+                            </Form>
                         </Slide>
-                        <Snackbar
-                            style={{textAlign: 'center'}}
-                            anchorOrigin={{ vertical: 'bottom', horizontal: 'center'}}
-                            open={submitSuccess}
-                            autoHideDuration={6000}
-                            onClose={this.handleSnackbarClose}
-                            SnackbarContentProps={{
-                                'aria-describedby': 'message-id',
-                            }}
-                            message={<span id="message-id">Thanks for reaching out!</span>}
-                            action={[
-                                <IconButton
-                                  key="close"
-                                  aria-label="Close"
-                                  color="inherit"
-                                  onClick={this.handleSnackbarClose}
-                                >
-                                  <CloseIcon />
-                                </IconButton>,
-                            ]}
-                        />
-
-                    </Form>
+                    </OnVisible>
                 </div>
             </div>
         );
