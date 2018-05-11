019d55d38cd796ee21181b8e4b6ba4837614e8fb
Ben Brewer <benbrewerbowman@gmail.com>
2018-05-10 23:42:06 -0600

    Mountains added to about section

--- src/_components/AboutPage/AboutPage.js
+++ src/_components/AboutPage/AboutPage.js
@@ -1,45 +1,68 @@
 import React from 'react';
+import Grid from 'material-ui/Grid';
+import ProfilePic from './about-img.png';
 
 
 const styles = {
 
     container: {
-        padding: '50px 0px'
+        padding: '50px 50px',
+        
     },
 
     header: {
         textAlign: 'center',
-        marginBottom: 30,
-        fontSize: 36
+        marginBottom: 20,
+        fontSize: 42
     },
 
-    form: {
+    content: {
+        fontSize: 18,
+        textAlign: 'justify',
+        textJustify: 'inter-word'
+    },
+
+    centerChildren: {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginBottom: 30
     },
 
-    field: {
-        width: '90vw',
-        maxWidth: 600
+    skillItem: {
+        maxHeight: 200
     },
 };
 
 
 const AboutPage = (props) => (
 
-    <div style={{backgroundColor: 'whiteSmoke'}} id={props.id} > 
+    <div id={props.id} > 
         <div style={styles.container} >
-            <h1 style={styles.header}> Hey! I'm Ben </h1>
-            <h3>
-                I am a software developer from Asheville, NC with a passion for working on new and exciting projects.
-                I graduated from University of North Carolina at Asheville, Summa Cum Laude, and shortly thereafter relocated to Salt Lake City, UT.
-                I am a fervent snowboarder, mountain biker, and backpacker, and enjoy just about anything outdoors.
-                You can catch me shredding some fresh powder in the winter, or drinking a cold one in the mountains in the summer.
-                Environmental sustainability and conservation are two important issues to me. Anything that helps us conserve and reduce our ecological footprint has both my interest and support.
-                We only have one earth, let's take care of it!
-            </h3>
+            <Grid container spacing={24} direction='row-reverse' alignItems='center' justify='center' > 
+                <Grid item xs={12} md={6} style={styles.centerChildren} >
+                    <img src={ProfilePic} style={{ width: 300, height: 300, paddingBottom: 12}} />
+                </Grid>           
+                <Grid item xs={12} md={6} style={styles.centerChildren} >
+                    <div>
+                    <h1 style={styles.header}> Hey! I'm Ben </h1>
+                        <h3 style={styles.content}>  
+                            I am a software developer from Asheville, NC with a passion for working on new and exciting projects.
+                            I graduated from University of North Carolina at Asheville, Summa Cum Laude in Mechatronics Engineering, and shortly thereafter relocated to Salt Lake City, UT.
+                            I am a fervent snowboarder, mountain biker, and backpacker, and enjoy just about anything outdoors.
+                            
+                            Environmental sustainability and conservation are two important issues to me. Anything that helps us conserve and reduce our ecological footprint has both my interest and support.
+                            We only have one earth, let's take care of it!
+                        </h3>
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
+        <div style={{marginTop: -50, height: 500, clipPath: 'polygon(0 20%, 100% 0, 100% 80%, 85% 50%, 60% 100%, 40% 60%, 25% 90%, 20% 80%, 15% 90%, 0 60%)', backgroundColor: '#5DDFED', background: 'linear-gradient(to top, #5DDFED 20%, Whitesmoke)', ...styles.container}} >
+            <h2 style={{marginTop: 100, textAlign: 'center'}}>  
+                You can catch me shredding some fresh powder in the winter... <br/> or drinking a cold one in the mountains in the summer!
+            </h2>
         </div>
     </div>
 );
