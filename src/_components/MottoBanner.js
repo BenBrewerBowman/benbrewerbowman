26f22e8c00b056bafc5c29b175f9aae8cbe6aa6a
Ben Brewer <benbrewerbowman@gmail.com>
2018-06-08 19:38:40 -0600

    Skills images moved to img directory

--- src/_components/MottoBanner.js
+++ src/_components/MottoBanner.js
@@ -5,25 +5,16 @@ import SunsetMountainsJPG from '../_img/sunset-mountains.jpg';
 const styles = {
 
     container: {
-        // textAlign: 'center',
         width: '100%',
         minWidth: 800,
         height: '100%',
         backgroundImage: `url(${SunsetMountainsJPG})`,
         backgroundSize: '100% 100%',
-        backgroundPosition: "center",
-        color: 'white',
-        // margin: 'auto'
-        // position: 'relative',
-        // left: '50%'
-        // padding: '30px 50px',
-        // backgroundColor: '#5DDFED',
+        backgroundPosition: "left",
+        color: 'white'  
     },
 
     centerContent: {
-        // position: 'relative',
-        // top: '50%',
-        // transform: 'translateY(-50%)'
         display: 'flex', 
         flexDirection: 'column',
         alignItems: 'center',
@@ -31,28 +22,14 @@ const styles = {
         width: '100vw',
         textAlign: 'center',
         margin: "0px",
-        // width: '100vw',
-        // maxWidth: 800,
-        // margin: 'auto',
         paddingTop: '30px',
         paddingBottom: '30px'
-
-        
-
-        // margin: 'auto'
     },
 
     content: {
         fontSize: 16,
         maxWidth: 800,
         padding: '0px 20px',
-        // textAlign: 'center',
-        
-        
-        // textJustify: 'inter-word',
-        // width: '100vw',
-        
-
         margin: 'auto'
     },
 };
