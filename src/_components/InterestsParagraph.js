cbdf60c54431dd31ce048dbf5f638e709039c00e
Ben Brewer <benbrewerbowman@gmail.com>
2018-06-10 01:22:20 -0600

    Style refactored interest paragraph

--- src/_components/InterestsParagraph.js
+++ src/_components/InterestsParagraph.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import SunsetMountainsJPG from '../_img/sunset-mountains.jpg';
+import { withStyles } from '@material-ui/core/styles';
 
 
-const styles = {
+const styles = theme => ({
 
     container: {
         width: '100%',
@@ -31,28 +32,31 @@ const styles = {
         padding: '0px 20px',
         margin: 'auto'
     },
+});
+
+
+const InterestsParagraph = (props) => {
+
+    const { classes } = props;
+    return (
+        <div className={classes.container} id={props.id} >
+            <div className={classes.centerContent} >
+                <h1 className={classes.header} > 
+                    When I'm not developing, I'm outside on some adventure!
+                </h1>
+                <p className={classes.content} >
+                    In my free time I enjoy snowboarding, mountain biking, hiking, backpacking, and visiting beautiful places.
+                    Living in Salt Lake City has afforded me great access to outdoor recreation, and I am very grateful to live in such a beautiful city.
+                    Maintaining an active lifestyle has played a role in some of my views on important issues.
+                    Environmental conservation and sustainability have become two very important issues to me.
+                    I enjoy learning about green technology that reduces our carbon footprint or improves our air and water quality.
+                    I think we owe it to our communities as well as future generations to not leave our surroundings in a worse state than we found it.
+                    We only have one earth, so let's all do our part in taking care of it!
+                </p>
+            </div>
+        </div>
+    );
 };
 
-
-
-
-const InterestsParagraph = (props) => (
-    <div id={props.id} style={styles.container} >
-        <div style={styles.centerContent}>
-            <h1 style={styles.text}> 
-                When I'm not developing, I'm outside on some adventure!
-            </h1>
-            <p style={styles.content}>
-                In my free time I enjoy snowboarding, mountain biking, hiking, backpacking, and visiting beautiful places.
-                Living in Salt Lake City has afforded me great access to outdoor recreation, and I am very grateful to live in such a beautiful city.
-                Maintaining an active lifestyle has played a role in some of my views on important issues.
-                Environmental conservation and sustainability have become two very important issues to me.
-                I enjoy learning about green technology that reduces our carbon footprint or improves our air and water quality.
-                I think we owe it to our communities as well as future generations to not leave our surroundings in a worse state than we found it.
-                We only have one earth, so let's all do our part in taking care of it!
-            </p>
-        </div>
-    </div>
-);
-
-export { InterestsParagraph };
+const InterestsParagraphWithStyles = withStyles(styles) (InterestsParagraph);
+export { InterestsParagraphWithStyles as InterestsParagraph };
